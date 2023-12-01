d218b055ebfcc4b3f2a5e141a627c97b9b932cfd
PzaThief <vwjdalsgkv@gmail.com>
2023-12-01 10:05:15 +0900

    apply icon and change design

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -5,10 +5,10 @@ import { Input } from "ui/input"
 export default function MainPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 p-4 md:p-6">
-      <section className="flex flex-col space-y-6 bg-white dark:bg-gray-800 rounded-lg shadow-md w-full max-w-md md:max-w-lg px-4 py-6">
-        <h1 className="text-2xl font-bold text-gray-800 dark:text-gray-200">Currency Converter</h1>
-        <div className="grid grid-cols-2 gap-4">
-          <div className="col-span-1 space-y-2">
+      <section className="flex flex-col bg-white dark:bg-gray-800 rounded-lg shadow-md w-full max-w-md md:max-w-lg px-4 py-6">
+        <h1 className="text-2xl font-bold text-gray-800 dark:text-gray-200">Gukbap Converter</h1>
+        <div className="grid grid-cols-2 gap-4 mb-4">
+          <div className="col-span-1 ">
             <Label htmlFor="from-currency">From</Label>
             <Select>
               <SelectTrigger aria-label="From currency" id="from-currency">
@@ -23,21 +23,21 @@ export default function MainPage() {
               </SelectContent>
             </Select>
           </div>
-          <div className="col-span-1 space-y-2">
+          <div className="col-span-1">
             <Label htmlFor="amount">Amount</Label>
             <Input id="amount" placeholder="Enter amount" type="number" />
           </div>
         </div>
         <div className="grid grid-cols-2 gap-4">
-          <div className="col-span-1 space-y-2 text-center">
-          Reverse (will be icon)
+          <div className="col-span-1 flex justify-center items-center" >
+            <img width="20px" height="20px" alt="invert" src={`${process.env.PUBLIC_URL}/sync-icon.svg`} />
           </div>
-          <div className="col-span-1 space-y-2 text-center text-lg font-bold">
-          =
+          <div className="col-span-1 flex justify-center items-center">
+            <img width="15px" height="15px" alt="equal" src={`${process.env.PUBLIC_URL}/equal-icon.svg`} />
           </div>
         </div>
         <div className="grid grid-cols-2 gap-4">
-          <div className="col-span-1 space-y-2">
+          <div className="col-span-1">
             <Label htmlFor="to-currency">To</Label>
             <Select>
               <SelectTrigger aria-label="To currency" id="to-currency">
@@ -52,14 +52,14 @@ export default function MainPage() {
               </SelectContent>
             </Select>
           </div>
-          <div className="col-span-1 space-y-2">
-          <Label htmlFor="amount">Exchanged Amount</Label>
-          <div className="col-span-1 mt-2 md:mt-6 bg-blue-100 dark:bg-blue-900 p-2 rounded-md text-blue-800 dark:text-blue-200">
-            -
-          </div>
+          <div className="col-span-1 mb-4">
+            <Label htmlFor="amount">Amount</Label>
+            <div className="bg-blue-100 dark:bg-blue-900 p-2 rounded-md text-blue-800 dark:text-blue-200 text-left">
+              -
+            </div>
           </div>
         </div>
-        <div className="space-y-4">
+        <div className="space-y-2">
           <h2 className="text-lg font-bold text-gray-800 dark:text-gray-200">Popular Exchanges</h2>
           <ul className="space-y-2">
             <li className="text-gray-600 dark:text-gray-300">USD to EUR</li>
